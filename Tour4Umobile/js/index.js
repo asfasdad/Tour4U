c67172dda34aabf1c92c295e603196daddada022
EyesiM <302821641@qq.com>
2016-04-17 13:35:08 +0800

    动画切换以及动画效果展示完成，整个网页基本完成

--- Tour4Umobile/js/index.js
+++ Tour4Umobile/js/index.js
@@ -17,8 +17,74 @@ $(document).ready(function() {
    		 	slidesNavigation: true,
    		 	loopHorizontal: false,
    		 	controlArrowColor: 'transparent',
-   		 	continuousVertical: true
-   		 });
-	});
+   		 	continuousVertical: true,
+                                          anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage','fifthPage','sixthPage', 'lastPage'],
+                                          afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex) {
+                                                if (slideIndex===1) {
+                                                    $('#int-slide').addClass("animation");
+                                                }
+                                                if (slideIndex===2) {
+                                                    $('#fb-slide').addClass("animation");
+                                                }
+                                                if (slideIndex===3) {
+                                                    $('#food-slide').addClass("animation");
+                                                }
+                                                if (slideIndex===4) {
+                                                    $('#play-slide').addClass("animation");
+                                                }
+                                                if (slideIndex===5) {
+                                                    $('#car-slide').addClass("animation");
+                                                }
+                                                if (slideIndex===6) {
+                                                    $('#horse-slide').addClass("animation");
+                                                }
+                                                if (slideIndex===7) {
+                                                    $('#inf-slide').addClass("animation");
+                                                }
 
+                                          },
+                                          onSlideLeave: function( anchorLink, index, slideIndex, direction, nextSlideIndex){
+                                                if (slideIndex===1) {
+                                                    $('#int-slide').removeClass("animation");
+                                                }
+                                                if (slideIndex===2) {
+                                                    $('#fb-slide').removeClass("animation");
+                                                }
+                                                if (slideIndex===3) {
+                                                    $('#food-slide').removeClass("animation");
+                                                }
+                                                if (slideIndex===4) {
+                                                    $('#play-slide').removeClass("animation");
+                                                }
+                                                if (slideIndex===5) {
+                                                    $('#car-slide').removeClass("animation");
+                                                }
+                                                if (slideIndex===6) {
+                                                    $('#horse-slide').removeClass("animation");
+                                                }
+                                                if (slideIndex===7) {
+                                                    $('#inf-slide').removeClass("animation");
+                                                }
+                                          }
+
+           		 });
+              });
+
+
+                // $('body').fullpage({
+                //     anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'fifthPage','sixthPage','lastPage'],
+                //     afterLoad: function(anchorLink, index){
+                //         var loadedSection = $(this);
+                //         //using index
+                //         if(index == 0){
+                //             alert("Section 3 ended loading");
+                //         }
+                //         //using anchorLink
+                //         if(anchorLink == 'secondSlide'){
+                //             alert("Section 2 ended loading");
+                //         }
+                //     }
+                // });
 }); 
+
+// 当前屏幕的动画
